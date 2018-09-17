aa7653f17804a1efaa45bb922b5f3dfe1aee7f82
K8Sewell <kait@kaitsewell.com>
2018-09-17 16:57:30 -0700

    added protected page and routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, RedirectProps } from 'react-router-dom'
 
 import Register from './pages/Register'
+import AuthService from './services'
+import Protected from './pages/Protected'
+import Public from './pages/Public'
 
 class App extends Component {
 	render() {
+		// creates new instance of AuthService class
+		let auth = new AuthService()
 		return (
 			<div>
 				<Router>
-					<Switch>
+				<Switch>
+					{(auth.loggedIn())
+					// if logged in
+					? <Switch>
+						<Route path="/public" component={Public} />
+						<Route path="/protected" component={Protected} />
 						<Route path="/register" component={Register} />
 					</Switch>
+					// if not logged in (ie Guest User)
+					: <Switch>
+						<Route path="/public" component={Public} />
+						<Redirect from="/protected" to="/register" />
+						<Route path="/register" component={Register} />
+					</Switch>}
 				</Router>
 			</div>
 		);
---
+++ src/pages/Protected.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import { Redirect, Link } from 'react-router-dom'
+
+
+class Protected extends Component {
+	
+
+	render() {
+		return () {
+            <div>
+                <h1>Protected Page</h1>
+            </div>
+        }
+
+    }
+
+
+export default Protected

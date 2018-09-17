82639868516634941ffb24c0c15da037fde5b2d6
K8Sewell <kait@kaitsewell.com>
2018-09-17 16:56:51 -0700

    added public page

---
+++ src/pages/Public.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import { Redirect, Link } from 'react-router-dom'
+
+
+class Public extends Component {
+	
+
+	render() {
+		return () {
+            <div>
+                <h1>Public Page</h1>
+            </div>
+        }
+
+    }
+
+
+export default Public
